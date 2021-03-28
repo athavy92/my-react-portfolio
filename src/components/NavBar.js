c44e70758b8593eb4918254c2f0cce7c1bfb60cf
athavy92 <athavy92@gmail.com>
2021-03-28 16:43:04 -0400

    install tailwind css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,12 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
   return (
-    <header className="bg-gray-900 bg-gradient-to-r from-black via-gray-700 to-blue-400 shadow-lg">
+    <header className="relative bg-gray-900 bg-gradient-to-r from-black via-gray-700 to-blue-400 shadow-lg">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
           <NavLink to="/" exact
             activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-4 text-black-700 hover:text-blue-800 text-4xl font-bold cursive tracking-widset">
+            className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-blue-800 text-4xl font-bold cursive tracking-widset">
             Home
           </NavLink>
           <NavLink to="/post"
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    boxShadow: {
+      // lg: '0 10px 15px -3px red, 0 4px 6px -2px red',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
